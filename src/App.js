fdb3d1168f4ca5cf7741422c0e37848f4db8062a
Marc Seiferle <marc.seiferle@swisscom.com>
2017-10-30 10:38:11 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ToDoPage from './ToDo/ToDoPage';
+
+class App extends Component {
+  render() {
+    return <ToDoPage />;
+  }
+}
+
+export default App;
---
+++ src/ToDo/ToDoPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ToDoPage = () => {
+  return <div>Start here...</div>;
+};
+
+export default ToDoPage;
